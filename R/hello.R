88bc5557c6d37e13f976131576dcc50d75f504c0
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2025-01-28 13:37:29 -0500

    updating package

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,15 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   https://r-pkgs.org
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
+#' Personal greeting
+#'
+#' @description Greet a person and appropriately capitalize their name.
+#'
+#' @param name Your name (character string; e.g. "john doe").
+#'
+#' @return A character string, capitalized to title case.
+#' @export
+#'
+#' @examples
+#' hello("james bond")
+hello <- function(name = "your name") {
+  name <- stringr::str_to_title(name)
+  print(paste("Hello,", name))
 }
