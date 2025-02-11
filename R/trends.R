7e665df5d3528cc4a0434da032775d9ebe48a5a2
Brendan Turley <brendan.turley@nmfs.local>
2025-02-11 15:34:36 -0500

    BDT test function

---
+++ R/trends.R
@@ -0,0 +1,16 @@
+#' List trend options
+#'
+#' @description List trend options
+#'
+#' @return A list of trend options
+#'
+#' @export
+#'
+#' @examples trends()
+#'
+
+trends <- function() {
+
+  print("Trend options:")
+
+}
