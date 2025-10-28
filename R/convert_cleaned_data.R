af271fd7dd33b5fe5a419059545542a7df9a19bb
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2025-10-28 14:10:13 -0400

    fixing convert cleaned data function to make indicator name the header row

--- R/convert_cleaned_data.R
+++ R/convert_cleaned_data.R
@@ -17,10 +17,10 @@
 #'   bio_spec2 = c(7, 3, 8, 8, 12)
 #' )
 #'
-#' # 2. Define ALL header components, starting with "" for the 'year' column
-#' indicator_names <- c("", "Biomass", "Biomass")
-#' unit_names <- c("", "Count", "Count")
-#' extent_names <- c("", "species A", "species B")
+#' # 2. Define header components for the data rows (ignore year)
+#' indicator_names <- c("Biomass", "Biomass")
+#' unit_names <- c("Count", "Count")
+#' extent_names <- c("species A", "species B")
 #'
 #' # 3. Call the function
 #' final_table <- convert_csv(dat, indicator_names, unit_names, extent_names)
@@ -29,25 +29,20 @@
 convert_cleaned_data <- function(data, indicator_names, unit_names, extent_names){
 
   num_cols <- ncol(data)
-  if (length(indicator_names) != num_cols || length(unit_names) != num_cols || length(extent_names) != num_cols) {
-    stop(paste("Error: All header vectors must have a length of", num_cols, "(the number of columns in the data)."))
-  }
-
-  if (indicator_names[1] != "" || unit_names[1] != "" || extent_names[1] != "") {
-    stop("Error: The first element of 'indicator_names', 'unit_names', and 'extent_names' must be an empty string (\"\") to leave the first column header blank.")
+  if (length(indicator_names) != num_cols-1 || length(unit_names) != num_cols-1 || length(extent_names) != num_cols-1) {
+    stop(paste("Error: All header vectors must have a length of", num_cols-1, "(the number of columns in the data minus the year column)."))
   }
 
   data_matrix <- as.matrix(data)
   colnames(data_matrix) <- NULL
 
   data_block <- rbind(
-    indicator_names,
-    unit_names,
-    extent_names,
+    c("unit", unit_names),
+    c("extent", extent_names),
     data_matrix
   )
 
-  colnames(data_block) <- NULL
+  colnames(data_block) <- c("indicator", indicator_names)
 
   formatted_data <- as.data.frame(data_block, stringsAsFactors = FALSE)
   rownames(formatted_data) <- NULL
