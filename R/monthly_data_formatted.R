278e541086144b94232b60b96a26ee75483ebf02
Brittany Troast <brittany.troast@noaa.gov>
2026-01-27 10:53:04 -0500

    update test data

---
+++ R/monthly_data_formatted.R
@@ -0,0 +1,14 @@
+#' Monthly multi metric formatted example data
+#'
+#' A list object containing simulated metric data for package examples. This example is for a monthly multi metric indicator (a vector of random numbers). This has been prepped using the data_prep function, for use with IEAnalyzeR's plotting function.
+#'
+#' @format A data frame with 132 rows and 5 variables:
+#' \describe{
+#'   \item{date}{A vector of months.}
+#'   \item{temp_a}{An vector of random numbers.}
+#'   \item{salinity_a}{A vector of random numbers.}
+#'   \item{temp_b}{An vector of random numbers.}
+#'   \item{salinity_b}{A vector of random numbers.}
+#' }
+#' @source Simulated data
+"monthly_data_formatted"
